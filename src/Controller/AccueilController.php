dbac6ecbff7b56253aa83bf2ffe43279786af8e1
Max Reis <maxreis507@gmail.com>
2026-01-08 16:34:29 +0100

    feat: add legal mentions

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,11 +8,18 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
+    #[Route(['/', 'accueil'], name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+        ]);
+    }
+    
+    #[Route(['mentions-legales'], name: 'legal_mentions')]
+    public function mentions(): Response
+    {
+        return $this->render('accueil/mentions-legales.html.twig', [
         ]);
     }
 }
